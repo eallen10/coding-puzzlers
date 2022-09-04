69b377ef7a429551cca2f9b2911d2a236fae2784
eallen10 <eallen42692@gmail.com>
2022-09-04 00:21:27 -0400

    Remove incorrect solution

--- puzzles/longest-increasing-subsequence/longest-increasing-subsequence.spec.ts
+++ puzzles/longest-increasing-subsequence/longest-increasing-subsequence.spec.ts
@@ -12,4 +12,8 @@ describe('longest increasing subsequence', () => {
     it('returns 1 when given [7,7,7,7,7,7,7]', () => {
         expect(trivialSolution([7,7,7,7,7,7,7])).toBe(1);
     });
+
+    it('returns 5 when given [10,9,10,11,1,2,3,4,5]', () => {
+        expect(trivialSolution([10,9,2,5,3,7,101,18])).toBe(5);
+    });
 });
