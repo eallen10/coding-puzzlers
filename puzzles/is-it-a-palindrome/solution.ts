3eea7f6350442558c1d9ccdb17646aba09e4a5f3
eallen10 <eallen42692@gmail.com>
2022-09-04 01:22:13 -0400

    Add two puzzles (#5)
    * cleanup dupe file
    
    * Remove incorrect solution
    
    * comment failing tests
    
    * Add two puzzles

---
+++ puzzles/is-it-a-palindrome/solution.ts
@@ -0,0 +1,12 @@
+function solution(sequence: number[]) {
+    for(let i = 0; i < sequence.length; i++) {
+        const mirrorIndex = sequence.length - i - 1;
+        if(sequence[i] !== sequence[mirrorIndex]) {
+            return false;
+        } else if(mirrorIndex - i === 1 || mirrorIndex - i === 2) {
+            return true;
+        }
+    }
+}
+
+export { solution }
---
+++ puzzles/roman-numeral-to-number/solution.ts
@@ -0,0 +1,33 @@
+export function solution(romanNumeral: string) {
+    const rnMap: any = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    const specialCases: any = {
+        'IV': 4,
+        'IX': 9,
+        'XL': 40,
+        'XC': 90,
+        'CD': 400,
+        'CM': 900
+    };
+    let runningTotal = 0;
+
+    for(let i=0; i < romanNumeral.length; i++) {
+        const currentChar = romanNumeral.charAt(i);
+        const specialCaseValue = specialCases[currentChar + romanNumeral.charAt(i+1)];
+        if(specialCaseValue) {
+            runningTotal += specialCaseValue;
+            i++;
+        } else {
+            runningTotal += rnMap[currentChar];
+        }
+    }
+
+    return runningTotal;
+}
